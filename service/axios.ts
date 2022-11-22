4799a7846c81f858a085e97f5c89186d0a0a23b9
Luca Alfaro Rampinelli <luca.alfaro.rampinelli@gmail.com>
2022-11-22 15:34:35 -0300

    public vercel env

--- service/axios.ts
+++ service/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = process.env.VERCEL_ENV! == 'production' ? 
+const baseURL = process.env.NEXT_PUBLIC_VERCEL_ENV! == 'production' ? 
   `https://${process.env.NEXT_PUBLIC_VERCEL_URL!}`:`http://localhost:3000`
 
 export const axiosClient = axios.create({
