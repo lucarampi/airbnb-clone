6bae1d289784c772ca995ad8cafaf00c39ecdabe
Luca Alfaro Rampinelli <luca.alfaro.rampinelli@gmail.com>
2022-11-22 18:20:55 -0300

    ultrawide responsive

--- app/Banner.tsx
+++ app/Banner.tsx
@@ -4,15 +4,16 @@ import Image from "next/image";
 
 export default function Banner() {
   return (
-    <div className="relative h-[200px] sm:h-[300px] md:h-[450px] transition-all">
+    <div className="relative object-contain h-[200px] sm:h-[300px] md:h-[450px] transition-all">
       <Image
-        className="overflow-hidden"
+        className="overflow-hidden [144px]:rounded-b-lg"
         src="/banner-crop-img.png"
         fill
         objectFit={"cover"}
         objectPosition={"center"}
         alt="logo"
       />
+      
       <div className="absolute items-center justify-center gap-2 flex flex-col top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-sm sm:text-lg">
         <span className="font-semibold">Not sure where to go? Perfect!</span>
         <button
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import ExploreNearby from "./ExploreNearby";
 export default async function Page() {
   return (
     <div
-    // className="flex flex-col gap-4"
+    className=" w-full mx-auto  max-w-[1440px]"
     >
       <Banner />
       <main className="max-w-7xl mx-auto px-8 sm:px-16 justify-self-start pt-4">
