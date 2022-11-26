afc91c949028e600db1a23fcfc3382e6db82a93f
Luca Alfaro Rampinelli <luca.alfaro.rampinelli@gmail.com>
2022-11-26 15:56:14 -0300

    updating button

--- app/LargeCard.tsx
+++ app/LargeCard.tsx
@@ -33,7 +33,7 @@ export default async function LargeCard() {
         <h3 className="text-4xl mb-3 w-64 capitalize">{cardData.title}</h3>
         <p>{cardData.description}</p>
         <button
-          className="text-sm shadow-sm hover:scale-105 active:scale-95 text-white bg-gray-900 px-4 py-2 rounded-lg mt-5 transition-all"
+          className="text-md shadow-sm hover:scale-105 active:scale-95 text-white bg-gray-900 px-4 py-2 rounded-lg mt-5 transition-all"
           type="button"
         >
           {cardData.buttonText}
